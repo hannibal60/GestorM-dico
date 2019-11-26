7a4d41493a6348ffeed821589b0fdc9c5a4721be
AJovanny <34175925+AJovanny@users.noreply.github.com>
2019-11-25 23:15:11 -0600

    Componente Médico Completo
    Agrego componente médico con funcionalidad completa
    
    Co-Authored-By: edgs5 <edgs5@users.noreply.github.com>

---
+++ Implementación/Componente Médico v1.2/Medicos/src/java/Servlet/modificaMedico.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+import metodos.CRUD;
+
+/**
+ *
+ * @author acer
+ */
+public class modificaMedico extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            String action = request.getParameter("Modifica");
+
+            String nombre = request.getParameter("Nombre");
+            String APaterno = request.getParameter("APaterno");
+            String AMaterno = request.getParameter("AMaterno");
+            String sexo = request.getParameter("sexo");
+            String curp = request.getParameter("CURP");
+            String rfc = request.getParameter("RFC");
+            String cedula = request.getParameter("cedula");
+
+            
+            
+            CRUD metodo = new CRUD();
+            if (metodo.modifica(cedula,nombre,APaterno,AMaterno,sexo,curp,rfc)) {
+                request.getRequestDispatcher("Medicos.jsp").forward(request, response);
+                JOptionPane.showMessageDialog(null, "Medico Modificado Correctamente");
+            }
+            
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
